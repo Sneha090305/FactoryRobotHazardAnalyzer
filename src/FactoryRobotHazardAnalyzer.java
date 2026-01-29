f8fb0aab96531e81d8f03dcf90318d75c15fe726
Sneha <snehangap@gmail.com>
2026-01-29 15:32:41 +0530

    [Sneha] : UC8: modular OOP design with RobotHazardAuditor class

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -2,32 +2,6 @@ import java.util.Scanner;
 
 public class FactoryRobotHazardAnalyzer {
 
-    public static double calculateHazardRisk(double armPrecision, int workerDensity, String machineryState)
-            throws RobotSafetyException {
-
-        if (armPrecision < 0.0 || armPrecision > 1.0) {
-            throw new RobotSafetyException("Error: Arm precision must be 0.0-1.0");
-        }
-
-        if (workerDensity < 1 || workerDensity > 20) {
-            throw new RobotSafetyException("Error: Worker density must be 1-20");
-        }
-
-        double machineRiskFactor;
-
-        if ("Worn".equals(machineryState)) {
-            machineRiskFactor = 1.3;
-        } else if ("Faulty".equals(machineryState)) {
-            machineRiskFactor = 2.0;
-        } else if ("Critical".equals(machineryState)) {
-            machineRiskFactor = 3.0;
-        } else {
-            throw new RobotSafetyException("Error: Unsupported machinery state");
-        }
-// placeholder for now
-        return ((1.0 - armPrecision) * 15.0) + (workerDensity * machineRiskFactor);
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -42,8 +16,10 @@ public class FactoryRobotHazardAnalyzer {
         System.out.println("Enter Machinery State (Worn/Faulty/Critical):");
         String machineryState = sc.nextLine();
 
+        RobotHazardAuditor auditor = new RobotHazardAuditor();
+
         try {
-            double risk = calculateHazardRisk(armPrecision, workerDensity, machineryState);
+            double risk = auditor.CalculateHazardRisk(armPrecision, workerDensity, machineryState);
             System.out.println("Robot Hazard Risk Score: " + risk);
         } catch (RobotSafetyException e) {
             System.out.println(e.getMessage());
